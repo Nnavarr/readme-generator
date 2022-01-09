27b5069a32a323a9be7b61694ba6293338bc94e9
eon <noe.navarro1019@gmail.com>
2022-01-08 17:29:58 -0700

    created array of questions to be answered by the user. Additionally, created writeToFile function logic

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,100 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    // project title
+    {
+        'readmeSection': 'Title',
+        'type': 'input',
+        'name': 'Title',
+        'message': "What is the project's name?"
+    },
+
+    // project description
+    {
+        'readmeSection': 'Description',
+        'type': 'input',
+        'name': 'Description',
+        'message': "What is the project's description?"
+    },
+
+    // project installation
+    {
+        'readmeSection': 'Installation',
+        'type': 'input',
+        'name': 'Installation',
+        'message': 'What steps are involved in installing the project?'
+    },
+
+    // projects usage
+    {
+        'readmeSection': 'Usage',
+        'type': 'input',
+        'name': 'Usage',
+        'message': 'Please provide instructions and examples for use'
+    },
+
+    // project license
+    // TODO: Create a badge for the license selected
+    {
+        'readmeSection': 'License',
+        'type': 'list',
+        'name': 'License',
+        'message': 'Please select a license for the project',
+        'licenseOptions': ['license1', 'license2']
+    },
+
+    // project contributing guidelines
+    {
+        'readmeSection': 'Contributing',
+        'type': 'input',
+        'name': 'Contributing',
+        'message': 'Any guidelines for contributors?'
+    },
+
+    // github name
+    {
+        'readmeSection': 'QuestionsGithub',
+        'type': 'input',
+        'name': 'QuestionsGithub',
+        'message': 'What is your github username?'
+    },
+
+    // developer email
+    {
+        'readmeSection': 'QuestionsEmail',
+        'type': 'input',
+        'name': 'QuestionsEmail',
+        'mesage': 'What is your email address?'
+    }
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // return a promise and write file
+    return new Promise((resolve, reject) =>{
+        fs.writeFile(`./dist/${fileName}`, data, err =>{
+            // if error, reject promise and let the user know
+            if(err){
+                reject(err);
+                return;
+            }
+            // if no error, resolve and create file
+            resolve({
+                ok: true,
+                message: `${fileName} file created!`
+            });
+        });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
